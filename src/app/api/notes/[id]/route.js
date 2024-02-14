e20ee077c033c822cce13762aac1023fdaeece45
tomwvwest <tomwvwest@gmail.com>
2024-02-14 15:40:43 +0000

    complete testing of a succesful note patch

--- src/app/api/notes/[id]/route.js
+++ src/app/api/notes/[id]/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../../lib/prisma";
 
-export async function GET(res, {params}){
-  const id = Number(params.id)
+export async function GET(req, { params }) {
+  const id = Number(params.id);
   if (isNaN(id)) return NextResponse.json("Bad request", { status: 400 });
 
   const note = await prisma.notes.findUnique({
@@ -12,6 +12,39 @@ export async function GET(res, {params}){
   });
 
   if (!note) return NextResponse.json("Note not found", { status: 404 });
-  
+
   return NextResponse.json({ note, status: 200 });
+}
+
+export async function PATCH(req, { params }) {
+  const id = Number(params.id);
+  if (isNaN(id)) return NextResponse.json("Bad request", { status: 400 });
+
+  const body = await req.json();
+
+  const noteToPatch = await prisma.Notes.findUnique({
+    where: {
+      note_id: id,
+    },
+  });
+
+  if (!noteToPatch) {
+    return NextResponse.json("Note not found", { status: 404 });
+  }
+
+  const keysToUpdate = Object.keys(body);
+
+  keysToUpdate.forEach((key) => {
+    noteToPatch[key] = body[key];
+  });
+
+  const patchedNote = await prisma.Notes.update({
+    where: {
+      note_id: id,
+    },
+    data: noteToPatch,
+  });
+
+  console.log(patchedNote);
+  return NextResponse.json({ patchedNote, status: 200 });
 }
